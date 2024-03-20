5d661f1518ddbdb7d3511dfbab90172793cb9bc3
Tharindu Senevirathna <tharindu9573@gmail.com>
2024-03-21 01:43:21 +0530

    Jwt Authentication was added

--- SubmitterService/index.js
+++ SubmitterService/index.js
@@ -3,11 +3,14 @@ const app = express();
 const amqp = require('amqplib');
 const swaggerUi = require('swagger-ui-express');
 const specs = require('./swagger.js');
+const authenticateJWT = require('./middleware.js');
+const jwt = require('jsonwebtoken');
 
 const port = 3200;
 
 const rabbitMQUrl = `amqp://${process.env.BASE_URL}:4201/`;;
 const queueName = 'submitter_queue';
+const secret = 'mysupersecret'
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use(express.json());
@@ -38,7 +41,7 @@ app.use(express.json());
  *       200:
  *         description: New joke was submitted
  */
-app.post('/submit', (req, res) => {
+app.post('/submit', authenticateJWT, (req, res) => {
     try {
         var { type_id, joke_text, punch_line  } = req.body;
         var jsonMessage = JSON.stringify({ type_id, joke_text, punch_line  });
@@ -47,6 +50,45 @@ app.post('/submit', (req, res) => {
     } catch (error) {
         res.status(500).send(`An error was occured while submitting the joke. Error: ${error.message}`);
     }
+});
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Auth]
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             username:
+ *               type: string
+ *             password:
+ *               type: string 
+ *     responses:
+ *       200:
+ *         description: Authenticated
+ */
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    if(username === process.env.USERNAME && password === process.env.PASSWORD){
+        const token = jwt.sign({ userId: username }, secret, { expiresIn: '1h' });
+        res.status(200).send(token);
+    }
+    else{
+        res.status(401).send('Unauthorized');
+    }
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send('An error occurred. Please try again later.');
 });
 
 async function sendToQueue(message) {
